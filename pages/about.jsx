dccb789cd994c3efab8dc1ae60328bae435095c8
ASaidOguz <ahmetsaid2136@gmail.com>
2022-10-07 21:04:36 +0300

    about page added

---
+++ pages/about.jsx
@@ -0,0 +1,15 @@
+
+import Link from "next/link"
+const About = () => {
+  return (
+    <>   
+    <div>
+    
+    <h1>This is about page!</h1>
+    </div>
+    <Link href="/">Return Buy me a Coffee</Link>
+    </>
+  )
+}
+
+export default About
