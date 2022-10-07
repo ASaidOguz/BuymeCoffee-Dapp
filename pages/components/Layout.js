2141c92af39f4b2eac9a9bce232743c751f6fd2b
ASaidOguz <ahmetsaid2136@gmail.com>
2022-10-07 21:05:21 +0300

    Layout component added

---
+++ pages/components/Layout.js
@@ -0,0 +1,21 @@
+
+import styles from "../../styles/Home.module.css"
+
+import Nav from "./Nav"
+
+const Layout = ({children}) => {
+  return (
+    <>
+    
+    <Nav/>
+    <div className={styles.container}>
+      <main className={styles.main}>
+      
+      {children}
+      </main>
+    </div>
+    </>
+  )
+}
+
+export default Layout
